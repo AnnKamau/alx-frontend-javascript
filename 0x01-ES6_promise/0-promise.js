23f9ef20b25bcee9ebe654a0560959dc356efbbe
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-04-24 09:42:18 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,8 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
+    
     setTimeout(() => {
-      resolve("mock api response");
+      resolve("Mock API response");
     }, 1000);
   });
 }
