43bf2898d0dbf20925ce49e87f17c01f9e89b0d0
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-04-26 22:33:43 +0300

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,15 @@
+const carSymbol = Symbol('Car');
 
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+  
+  cloneCar() {
+    const clonedCar = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+    clonedCar[carSymbol] = true;
+    return clonedCar;
+  }
+}
