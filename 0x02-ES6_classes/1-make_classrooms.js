fa5a103c5a87bcc0064b0aa28975946b6eb0b956
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-04-26 22:39:48 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-import ClassRoom form "./0-classroom.js";
+import ClassRoom from "./0-classroom.js";
 
-export default function intializeRooms() {
+export default function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34)
-    ];
+  ];
 }
