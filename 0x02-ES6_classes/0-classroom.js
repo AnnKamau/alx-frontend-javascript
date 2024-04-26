5e7f6d58d9bd72d89be2ca27c08ce79e02de6573
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-04-26 22:40:26 +0300

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
-  constructor(masStudentsSize) {
-    this._masStudentsSize = masStudentsSize;
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
