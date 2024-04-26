b6cc4f3faf6308a6af7b337dfc2ca06f71b6e30a
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-04-26 22:37:47 +0300

    Update 7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,15 +3,15 @@ export default class Airport {
     this._name = name;
     this._code = code;
   }
-  
+
   get name() {
     return this._name;
   }
-  
+
   get code() {
     return this._code;
   }
-  
+
   toString() {
     return `Airport [${this.code}] { _name: '${this.name}', _code: '${this.code}' }`;
   }
