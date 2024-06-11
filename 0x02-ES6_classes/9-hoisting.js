5ce880c8d4c0c0bf7b550a0e69346dce0bf0150d
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-11 16:57:08 +0300

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-// Define the HolbertonClass first
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -14,11 +13,6 @@ export class HolbertonClass {
   }
 }
 
-// Then create instances of HolbertonClass
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-// Define the StudentHolberton class
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -39,17 +33,13 @@ export class StudentHolberton {
   }
 }
 
-// Create instances of StudentHolberton
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export the list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
-
-// Test the output (optional)
-console.log(listOfStudents);
-const listPrinted = listOfStudents.map(student => student.fullStudentDescription);
-console.log(listPrinted);
