985d407eb29c114ced4f42de858ea4bab893b9b8
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-11 16:10:13 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,16 +1,17 @@
 // 1-make_classrooms.js
-import { ClassRoom } from './0-classroom.js';
+import ClassRoom from './0-classroom.js';
 
-/**
- * Initializes an array of ClassRoom objects with specified sizes.
- * @return {ClassRoom[]} Array of ClassRoom objects.
- */
 function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34)
   ];
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+
+  return [room1, room2, room3];
 }
 
 export default initializeRooms;
