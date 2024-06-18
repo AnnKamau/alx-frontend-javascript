ad4383ff7a9e28770a19a92f37c108e3a626085b
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-18 08:48:07 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,16 +2,8 @@
 import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
-  ];
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-
-  return [room1, room2, room3];
+  const roomSizes = [19, 20, 34];
+  return roomSizes.map(size => new ClassRoom(size));
 }
 
 export default initializeRooms;
