1a0bd97a084380aa4ab02912367ca0678d4927ee
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-19 17:07:54 +0300

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,4 +1,4 @@
-import Car from "./10-car.js";
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
@@ -6,13 +6,7 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
-
-  cloneCar() {
-    const clonedCar = super.cloneCar();
-    Object.setPrototypeOf(clonedCar, Car.prototype);
-    return clonedCar;
+  static get [Symbol.species]() {
+    return Car;
   }
 }
