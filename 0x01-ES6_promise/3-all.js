04811413ec539ea06f28e1aeecf70a687cbc357a
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-19 16:37:39 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-      .then(([photo, user]) => {
-        console.log(`${user.firstName} ${user.lastName}`);
-      })
-      .catch(() => {
-        console.log('Signup system offline');
-      });
+    return Promise.all([uploadPhoto(), createUser()])
+        .then(([photo, user]) => {
+            console.log(`${user.firstName} ${user.lastName}`);
+        })
+        .catch(() => {
+            console.log('Signup system offline');
+        });
 }
 
 export default handleProfileSignup;
