31de6c13b18ccfd992b4c851b0906a34ee96e62c
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-19 16:42:03 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,16 @@
-// 6-final-user.js
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const signUpPromise = signUpUser(firstName, lastName);
-    const uploadPromise = uploadPhoto(fileName);
-
-    return Promise.allSettled([signUpPromise, uploadPromise])
-        .then((results) => results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
-        })));
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const result = [];
+  return Promise.allSettled(promises).then((results) => {
+    results.map(({ status, value, reason }) => (
+      result.push({
+        status,
+        value: status === 'rejected' ? reason.toString() : value,
+      })
+    ));
+    return result;
+  });
 }
